bbe41107f46b56d8ad713c2047bf8eda185a6d42
mawenjing <mawj2022@qq.com>
2024-04-29 19:07:06 +0800

    [init] Init AutoLink App

---
+++ src/main/java/cc/nihilism/app/autolink/AutoLinkApplication.java
@@ -0,0 +1,13 @@
+package cc.nihilism.app.autolink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/nihilism/app/autolink/AutoLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.nihilism.app.autolink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
