718ead8fe324fc7333b8f9bc99ac9153fa5699cb
starail <mawj2022@qq.com>
2024-05-17 21:24:10 +0800

    [feat] 重命名预览、多语言外挂字幕处理

--- src/main/java/cc/nihilism/app/autolink/service/filemonitor/controller/FileObserveController.java
+++ src/main/java/cc/nihilism/app/autolink/service/filemonitor/controller/FileObserveController.java
@@ -4,10 +4,16 @@ import cc.nihilism.app.autolink.basic.HttpResult;
 import cc.nihilism.app.autolink.service.filemonitor.domain.FileObserve;
 import cc.nihilism.app.autolink.service.filemonitor.service.FileObserveService;
 import cc.nihilism.app.autolink.service.filemonitor.utils.FormatFileNameUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/file-observe")
 public class FileObserveController {
@@ -42,9 +48,29 @@ public class FileObserveController {
     }
 
     @PostMapping("/rename")
-    public HttpResult renameMa10p(String targetPath, String fileName, int year, int season, String prefix, String suffix) {
+    public HttpResult rename(String targetPath, String fileName, int year, int season, String prefix, String suffix) throws IOException {
         List<String> res = FormatFileNameUtil.rename(targetPath, fileName, year, season, prefix, suffix);
 
         return HttpResult.data(res);
+    }
+
+    @PostMapping("/preview")
+    public HttpResult preview(String targetPath, String fileName, int year, int season, String prefix, String suffix) {
+        List<Map<String, String>> res = FormatFileNameUtil.preview(targetPath, fileName, year, season, prefix, suffix);
+
+        return HttpResult.data(res);
+    }
+
+    @PostMapping("/scan")
+    public HttpResult scan(String targetPath) {
+        File directory = new File(targetPath);
+        File[] files = directory.listFiles();
+        if (files == null || files.length == 0) {
+            return HttpResult.error("目标目录不存在");
+        }
+
+        List<String> fileNames = Arrays.stream(files).map(File::getName).toList();
+
+        return HttpResult.data(fileNames);
     }
 }
--- src/main/java/cc/nihilism/app/autolink/service/filemonitor/utils/FormatFileNameUtil.java
+++ src/main/java/cc/nihilism/app/autolink/service/filemonitor/utils/FormatFileNameUtil.java
@@ -2,29 +2,34 @@ package cc.nihilism.app.autolink.service.filemonitor.utils;
 
 import cc.nihilism.app.autolink.basic.exception.ServiceException;
 import cc.nihilism.app.autolink.basic.utils.StrUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class FormatFileNameUtil {
 
-    public static List<String> rename(String targetPath, String fileName, int year, int season) {
+    public static List<String> rename(String targetPath, String fileName, int year, int season) throws IOException {
         return rename(targetPath, fileName, year, season, "", "");
     }
 
-    public static List<String> rename(String targetPath, String fileName, int year, int season, String prefix, String suffix) {
-        File directory = new File(targetPath);
-        File[] files = directory.listFiles();
-
-        if (!directory.exists() || !directory.isDirectory() || files == null) {
+    public static List<String> rename(String targetPath, String fileName, int year, int season, String prefix, String suffix) throws IOException {
+        File originDir = new File(targetPath);
+        if (!originDir.exists() || !originDir.isDirectory()) {
             throw new ServiceException("目标文件夹不存在");
+        }
+
+        File directory = new File(originDir.getParent() + "/" + fileName + "." + year + ".S" + String.format("%02d", season));
+        FileUtils.moveDirectory(originDir, directory);
+
+        File[] files = directory.listFiles();
+        if (files == null || files.length == 0) {
+            throw new ServiceException("目标文件夹为空");
         }
 
         Arrays.sort(files, (o1, o2) -> {
@@ -39,15 +44,18 @@ public class FormatFileNameUtil {
         });
 
         List<String> res = new ArrayList<>();
-        Map<String, List<File>> map = Arrays.stream(files).filter(File::isFile).collect(Collectors.groupingBy(e -> FilenameUtils.getExtension(e.getPath())));
+        Map<String, List<File>> map = Arrays.stream(files).filter(File::isFile).collect(Collectors.groupingBy(FormatFileNameUtil::getFileExtension));
         map.forEach((k, v) -> {
             int count = 1;
             for (File file : v) {
+                String extName = FilenameUtils.getExtension(file.getPath());
+
                 // 转换文件名
-                String targetName = format(fileName, year, season, count++, prefix, suffix);
+                String targetName = format(fileName, year, season, count, prefix, suffix);
+                count++;
 
                 // 输出结果
-                targetName = "/" + FilenameUtils.getPath(file.getAbsolutePath()) + targetName;
+                targetName = "/" + FilenameUtils.getPath(file.getAbsolutePath()) + targetName + "." + extName;
                 res.add(targetName);
 
                 // 重命名文件
@@ -57,7 +65,7 @@ public class FormatFileNameUtil {
                         FileUtils.moveFile(file, new File(targetName));
                     }
                 } catch (IOException e) {
-                    throw new RuntimeException(e);
+                    throw new ServiceException("重命名文件失败");
                 }
             }
         });
@@ -75,13 +83,82 @@ public class FormatFileNameUtil {
         String format = fileName + "." + year + "." + seasonStr + "." + episodeStr;
 
         if (StrUtil.isNotEmpty(prefix)) {
-            format = prefix + " " + format;
+            format = prefix + format;
         }
 
         if (StrUtil.isNotEmpty(suffix)) {
-            format = format + " " + suffix;
+            format = format + suffix;
         }
 
         return format;
+    }
+
+    public static List<Map<String, String>> preview(String targetPath, String fileName, int year, int season, String prefix, String suffix) {
+        File directory = new File(targetPath);
+        if (!directory.exists() || !directory.isDirectory()) {
+            throw new ServiceException("目标文件夹不存在");
+        }
+
+        File[] files = directory.listFiles();
+        if (files == null || files.length == 0) {
+            throw new ServiceException("目标文件夹为空");
+        }
+
+        List<Map<String, String>> res = new ArrayList<>();
+        Map<String, String> moveDir = new HashMap<>();
+        moveDir.put("target", directory.getParent() + "/" + fileName);
+        moveDir.put("origin", targetPath);
+        res.add(moveDir);
+
+        Arrays.sort(files, (o1, o2) -> {
+            String o1Suffix = FilenameUtils.getExtension(o1.getPath());
+            String o2Suffix = FilenameUtils.getExtension(o2.getPath());
+            int suffixCompare = o1Suffix.compareTo(o2Suffix);
+            if (0 == suffixCompare) {
+                return o1.getName().compareTo(o2.getName());
+            }
+
+            return suffixCompare;
+        });
+
+        Map<String, List<File>> map = Arrays.stream(files).filter(File::isFile).collect(Collectors.groupingBy(FormatFileNameUtil::getFileExtension));
+        map.forEach((k, v) -> {
+            int count = 1;
+            for (File file : v) {
+                String extName = FilenameUtils.getExtension(file.getPath());
+
+                // 转换文件名
+                String targetName = format(fileName, year, season, count, prefix, suffix);
+                count++;
+
+                // 输出结果
+                targetName = "/" + FilenameUtils.getPath(file.getAbsolutePath()) + targetName + "." + extName;
+
+                Map<String, String> moveFile = new HashMap<>();
+                moveFile.put("target", targetName);
+                moveFile.put("origin", file.getName());
+                res.add(moveFile);
+            }
+        });
+
+        return res;
+    }
+
+    public static String getFileExtension(File file) {
+        String extension = FilenameUtils.getExtension(file.getPath());
+        if ("ass".equals(extension)) {
+            String name = file.getName();
+            name = name.substring(0, name.lastIndexOf("."));
+            extension = name.substring(name.lastIndexOf(".") + 1) + ".ass";
+        }
+
+        return extension;
+    }
+
+    public static void main(String[] args) {
+        String name = "a.b.c.d.ass";
+        File file = new File(name);
+        System.out.println(getFileExtension(file));
+        file.delete();
     }
 }
